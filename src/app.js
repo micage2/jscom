aeea6f71eb90c4cb1f8720d99fd8e3087f38678d
Michael Gehricke <michael.gehricke@mail.de>
2026-02-15 05:56:53 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,20 +9,24 @@ const left_right = DOM.create(SPLIT_LR, { ratio: 0.2 });
 const lr2 = DOM.create(SPLIT_LR, { ratio: 0.5 });
 const top_bottom = DOM.create(SPLIT_TB, { ratio: 0.1 });
 const tb2 = DOM.create(SPLIT_TB, { ratio: 0.7 });
+const tb3 = DOM.create(SPLIT_TB, { ratio: 0.4 });
 
 const p1 = DOM.create(DUMMY, { color: '#1a1916', text: 'P1' });
 const p2 = DOM.create(DUMMY, { color: '#2a1a16', text: 'P2' });
 const p3 = DOM.create(DUMMY, { color: '#1a1916', text: 'P3' });
 const p4 = DOM.create(DUMMY, { color: '#1a1916', text: 'P4' });
 const p5 = DOM.create(DUMMY, { color: '#1a1916', text: 'P5' });
+const p6 = DOM.create(DUMMY, { color: '#1a1916', text: 'P6' });
+
+tb3.setTop(p4).setBottom(p5);
 
 top_bottom.setTop(p2);
-top_bottom.setBottom(lr2.setLeft(p4).setRight(p5));
-
+top_bottom.setBottom(lr2.setLeft(p6).setRight(tb3));
 
 left_right.setLeft(tb2.setTop(p1).setBottom(p3));
 left_right.setRight(top_bottom);
 
 DOM.mount(left_right);
+
 
 
